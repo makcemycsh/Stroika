e04937826b0b45e17d63ea67c84d7d5b592fc92d
makcemycsh <m.shilo.v@yandex.ru>
2017-02-26 21:51:40 +0300

    footer

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Стройка</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <!--	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, maximum-scale=1.0"/>
+		-->
+    <title>Стройка</title>
 </head>
+
 <body>
     <header>
-    <h1>Привет</h1>
-    <h1>Привет, Макс!</h1>
+        <h1>Привет</h1>
+        <h1>Привет, Макс!</h1>
     </header>
     <content>
     </content>
     <footer>
+        <h1>Здесь будет футер</h1>
+        <h2>Привет, футер!</h2>
     </footer>
 </body>
+
 </html>
