654b36fa4bc2de4bc2432d8f92115fbff9351e5e
makcemycsh <m.shilo.v@yandex.ru>
2017-02-26 17:47:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Стройка</title>
+</head>
+<body>
+    <header>
+    <h1>Привет</h1>
+    </header>
+    <content>
+    </content>
+    <footer>
+    </footer>
+</body>
+</html>
